6d756291214851090e8d9c78623c768fe3dae6d7
clintmann <clintmann3@gmail.com>
2019-02-18 18:10:40 -0500

    adding file

---
+++ exch-status/get_status.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+"""
+VMO3 - User Status Microservice
+Author: Clint Mann
+
+Description:
+This is the user status microservice, it will
+ - reach out out to mailsetting.py to check the
+ automaticRepliesSetting for the user mailbox
+ - it will then send the results to VMO3 Hub microservice
+"""
+import requests
+from mail_setting import auto_reply
+
+
+def usr_status(token):
+    db = {
+        "usrs": [
+            {
+                "email": "clint@karmatek.io",
+                "monitor": True
+            },
+            {
+                "email": "marty@karmatek.io",
+                "monitor": True
+            },
+            {
+                "email": "chris@karmatek.io",
+                "monitor": False
+            }
+        ]
+    }
+
+    email_status ={}
+
+    # print(db)
+    # print(len(db))
+    # using dict.items()
+    count = 0
+    for key, value in db.items():
+        if isinstance(value, list):  # this will count the num of dict in list 'usrs'
+            count += len(value)
+
+    if count > 0:  # there are users in the list
+        for key, value in db.items():
+            for i in value:  # this will print the email addresses
+                email_address = i['email']
+                monitor = i['monitor']
+
+                if monitor is True:
+                    print(email_address, monitor)
+                    # print(monitor)
+                    user_status = auto_reply(token, email_address)
+                    if user_status != 'disabled':
+                        user_status = "True"
+                        # print(usr_status)
+                        print("User {0} has the following OoO status {1}".format(email_address, user_status))
+                        if email_address in email_status: # and value == email_status[email_address]:
+                            print('email address in db')
+                            if user_status in email_status: # and value == email_status[usr_status]:
+                                print('status is a match')
+                            else:
+                                email_status['status'] = user_status
+                        else:
+                            email_status['email'] = email_address
+                            email_status['status'] = user_status
+                    else:
+                        print("User {0} does not have an active Out of Office alert".format(email_address))
+
+                else:
+                    print("This User {0} is not in the monitor state".format(email_address))
+    else:
+        print("There are currently no users in database")
+
+    # check db data for email address and if monitor is true - if so add to list to check
+    # pass this list to mailbox function this will be variable email_addrs
+    # mailboxsettings(token, email_addr_list)
+
+    # POST TO API
+    print(email_status)
+
+    hub_url = "http://24.239.120.11:10001/api/setstatus"
+
+    payload = "{\n\t\"email\":\"clint@karmatek.io\",\n\t\"status\":\"True\"\n\t\n}"
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(hub_url, data=payload, headers=headers)
+
+    print(response.text)
+    #data = response.json()
+    #print(str(data))
+
+
---
+++ exch-status/mail_setting.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""
+VMO3 - AutoReply Microservice
+Author: Clint Mann
+
+Description:
+This is the token microservice, it will
+ - check the automaticRepliesSetting for the user mailbox
+"""
+import sys
+import requests
+
+
+def auto_reply(tkn, email_addr):
+    # for loop here - loop through email_addr list - and make a GET request for each
+    # If status is enabled - add to status list - this list will be sent back to the
+    # app Chris created
+
+    mailbox_url = "https://graph.microsoft.com/v1.0/users/" + email_addr + str(
+        "/mailboxSettings/automaticRepliesSetting")
+
+    payload = ""
+    headers = {
+        'Authorization': "Bearer " + str(tkn),
+        'cache-control': "no-cache"
+    }
+
+    try:
+        # response = requests.request("GET", mailbox_url, data=payload, headers=headers)
+        response = requests.get(mailbox_url, data=payload, headers=headers)
+        resp_json = response.json()
+        odata = resp_json['@odata.context']
+        int_reply = resp_json['internalReplyMessage']
+        ext_reply = resp_json['externalReplyMessage']
+        status = resp_json['status']
+
+        # print(odata)
+        # print(status)  # will return scheduled or disabled or alwaysEnabled
+        # print(int_reply)
+        # print(ext_reply)
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
+
+    return status
