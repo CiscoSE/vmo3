822ed3128946498a935fddc380af4c28c7a51256
clintmann <clintmann3@gmail.com>
2019-02-19 17:12:02 -0500

    updating

--- exch-status/mediator_listen.py
+++ exch-status/mediator_listen.py
@@ -1,18 +1,30 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
-'''
-Work in progress....
-'''
+"""
+VMO3 - MEDIATOR LISTEN Microservice
+Author: Clint Mann
+
+Description:
+This is the MEDIATOR LISTEN API microservice, it will
+ + wait for a REST API POST that contains user email address
+ and monitor status. It will then pass that information to
+ get_status.py to check the automaticRepliesSetting
+ for the user mailbox
+"""
 
 from flask import Flask, request
 from flask_restplus import Api, Resource
 import jsonify
+import json
+
 
 app = Flask(__name__)
 #api = Api(app) # version='1.0',title='VMO3 REGISTER USE API', default='VMO3',default_label='VMO3 namespace', description='VMO3 API to register users')
 
 
 USER = {}
+
+print('mediator listen - started')
 
 
 @app.route('/', methods=['GET'])
@@ -23,32 +35,34 @@ def home():
 
 @app.route('/users', methods=['GET'])
 def getusers():
-    req_data = request.get_json(force=True, silent=True)
-    # email = request.args.get('email')
-    email = req_data['email']
-    print(email)
-    status = req_data['status']
-    # status = request.args.get('status')
-    print(status)
-    USER['email'] = email
-    USER['status'] = status
-    print('users', USER)
-    return '''<h1>You would like to monitor user {} {}</h1>'''.format(email, status)
+    global USER
+    data = json.dumps(USER)
+    return data
 
 
 @app.route('/monitor', methods=['POST'])
 def monitorusers():
+    global USER
+    if not request.is_json:
+        return jsonify({"result": "Not JSON"}), 400
+
     req_data = request.get_json(force=True, silent=True)
-    # email = request.args.get('email')
-    email = req_data['email']
-    print(email)
-    status = req_data['status']
-    # status = request.args.get('status')
-    print(status)
-    USER['email'] = email
-    USER['status'] = status
-    print('users', USER)
-    return '''<h1>You would like to monitor user {} {}</h1>'''.format(email, status)
+
+    try:
+        email = req_data['email']
+        # print(email)
+        status = req_data['status']
+        # status = request.args.get('status')
+        # print(status)
+        USER['email'] = email
+        USER['status'] = status
+        print('users', USER)
+        data = json.dumps(USER)
+
+        return '''<h1>You would like to monitor user {} {}</h1>'''.format(email, status)
+
+    except(KeyError, TypeError, ValueError):
+        return jsonify({"result":"Invalid JSON"}), 400
 
 
 if __name__ == '__main__':
