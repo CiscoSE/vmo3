b10c3c09faba59f36691b26be71ca588c85e1f54
clintmann <clintmann3@gmail.com>
2019-02-22 11:10:11 -0500

    added error checking

--- exch-status/get_status.py
+++ exch-status/get_status.py
@@ -12,7 +12,7 @@ This is the user status microservice, it will
 """
 
 from mail_setting import auto_reply, graph_check_user
-from api import mediator_post , listener_get, listener_reset
+from api import mediator_post, listener_get, listener_reset
 
 
 def usr_status(token, med_url, listen_api_url, mailbox_base_url):
@@ -33,44 +33,46 @@ def usr_status(token, med_url, listen_api_url, mailbox_base_url):
         monitor = monitor_user['status']
         print('CHECK GRAPH FOR EMAIL: ', email_address)
         all_users = graph_check_user(token, mailbox_base_url)
-        print('ALL USERS FROM GRAPH: ', all_users)
-        if email_address in all_users.values():  # check if email account exists - NOT WORKING YET
-            if monitor == 'True':  # user supposed to be monitored
-                print(email_address, monitor)
-                # print(monitor)
-                user_status, message = auto_reply(
-                    token, email_address, mailbox_base_url)  # get status from MS Graph
 
-                if user_status != 'disabled':  # this means autoReply setting is enabled in some way
-                    user_status = "True"
-                    # print(usr_status)
-                    print("User {0} has the following OoO status {1} {2}"
-                          .format(email_address, user_status, message))
-                    if email_address in monitor_user.values():
-                        print('email address in db')
-                        if user_status in monitor_user.values():
-                            print('status is a match')
+        for element in all_users['value']:   # check if email account exists - NOT WORKING YET
+            if element['mail'] == email_address:
+                print("USER {0} FOUND in MS GRAPH".format(email_address))
+
+                if monitor == 'True':  # user supposed to be monitored
+                    print(email_address, monitor)
+                    # print(monitor)
+                    user_status, message = auto_reply(
+                        token, email_address, mailbox_base_url)  # get status from MS Graph
+
+                    if user_status != 'disabled':  # this means autoReply setting is enabled in some way
+                        user_status = "True"
+                        # print(usr_status)
+                        print("User {0} has the following OoO status {1} {2}"
+                              .format(email_address, user_status, message))
+                        if email_address in monitor_user.values():
+                            print('email address in db')
+                            if user_status in monitor_user.values():
+                                print('status is a match')
+                            else:
+                                monitor_user['status'] = user_status
                         else:
+                            monitor_user['email'] = email_address
                             monitor_user['status'] = user_status
+                            monitor_user['message'] = message
+
+                            print('email status ', monitor_user)
+                            print('message', message)
+                            print('POST OoO Status to Mediator Server...')
+                            mediator_post(med_url, monitor_user)
+                            print('POST complete...')
                     else:
-                        monitor_user['email'] = email_address
-                        monitor_user['status'] = user_status
-                        monitor_user['message'] = message
-
-                        print('email status ', monitor_user)
-                        print('message', message)
-                        print('POST OoO Status to Mediator Server...')
-                        mediator_post(med_url, monitor_user)
-                        print('POST complete...')
+                        print("User {0} does not have an active Out of Office alert"
+                              .format(email_address))
                 else:
-                    print("User {0} does not have an active Out of Office alert"
+                    print("This User {0} is not in the monitor state"
                           .format(email_address))
-
             else:
-                print("This User {0} is not in the monitor state"
-                      .format(email_address))
-        else:
-            print("USER {0} not found in MS GRAPH".format(email_address))
+                print("USER {0} NOT FOUND in MS GRAPH".format(email_address))
     else:
         print("There are currently no users in database")
 
